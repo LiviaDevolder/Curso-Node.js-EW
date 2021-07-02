dbd118e8595f6afeb5ebabd4b8f69901f652cbdd
LiviaDevolder <liviadevolder@gmail.com>
2021-07-02 15:21:27 -0300

    Modulo 2

---
+++ Modulo-2/for-forin/3.filter.js
@@ -0,0 +1,55 @@
+const { obterPessoas } = require('./service')
+
+/*
+
+ const item = {
+   nome: 'Livia',
+   idade: 18
+ }
+
+ const { nome } = item
+ console.log(nome)
+*/
+
+Array.prototype.meuFilter = function (callback) {
+  const lista = []
+  for(index in this) {
+    const item = this[index]
+    const result = callback(item, index, this)
+    // 0, "", null, undefined === false
+    if(!result) continue;
+    lista.push(item)
+  }
+  return lista;
+}
+
+async function main() {
+  try {
+    const {
+      results
+    } = await obterPessoas('a')
+
+    // const familiaLars = results.filter(function (item) {
+    //   // Por padrão, precisa retornar um booleano para
+    //   // informar se deve manter ou remover da lista
+    //   // false -> remove da lista
+    //   // true -> mantém
+    //   // não encontrou = -1
+    //   // encontrou = posicaoNoArray
+
+    //   const result = item.name.toLowerCase().indexOf(`lars`) !== -1
+    //   return result
+    // })
+    const familiaLars = results.meuFilter((item, index, lista) => {
+      console.log(`index: ${index + 1}`, lista.length)
+      return item.name.toLowerCase().indexOf('lars') !== -1
+    })
+
+    const names = familiaLars.map((pessoa) => pessoa.name)
+    console.log(names)
+
+  } catch (error) {
+    console.error('Algo deu errado', error)
+  }
+}
+main()
